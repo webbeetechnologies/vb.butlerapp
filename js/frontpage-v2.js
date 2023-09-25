0be57f9255d0105e1d43e19ed57b0766163aff68
bondan mintardjo <bondan@senta.nu>
2023-09-25 16:52:18 +0300

    js prev next

--- js/frontpage-v2.js
+++ js/frontpage-v2.js
@@ -299,13 +299,13 @@ $(document).ready(function () {
     /*=======================================================================================
     * INDUSTRIES: ELEMENTOR CAROUSEL PREV NEXT FAKE BUTTON
     =======================================================================================*/
-    $('.carousel-container .carousel-controller .prev a').on('click', function(e) {
+    $('.industries-section .carousel-controller .prev a').on('click', function(e) {
         e.preventDefault();
-        $(this).parents('.carousel-container').find('.carousel-prev').click();
+        $(this).parents('.industries-section').find('.carousel-prev').click();
     });
-    $('.carousel-container .carousel-controller .next a').on('click', function(e) {
+    $('.industries-section .carousel-controller .next a').on('click', function(e) {
         e.preventDefault();
-        $(this).parents('.carousel-container').find('.carousel-next').click();
+        $(this).parents('.industries-section').find('.carousel-next').click();
     });
     /*=======================================================================================
     * FAQs Section
