be20efe1fa4c06a941606b756ea71301fa5aee0c
bondan mintardjo <bondan@senta.nu>
2023-09-27 11:24:45 +0300

    testimonial change order of slick dots add new acf rating image

--- partials/testimonials_carousel_v2.php
+++ partials/testimonials_carousel_v2.php
@@ -17,6 +17,7 @@
 		$item = array(
 			'title' => get_the_title(),
 			'customer_picture' => get_field('customer_picture'),
+			'rating_image' => get_field('rating_image'),
 			'position' => get_field('position'),
 			'organisation_name' => get_field('organisation_name'),
 			'organisation_url' => get_field('organisation_url'),
@@ -46,8 +47,14 @@
 			<?php $title = $testi['title']; ?>
 			<?php $position = $testi['position']; ?>
 			<?php $content = $testi['content']; ?>
+			<?php $rating_image = $testi['rating_image'] ? $testi['rating_image']['url'] : ""; ?>
 
 			<div class="item">
+				<?php if ($rating_image != ""): ?>
+					<div class="rating">
+						<img src="<?php echo $rating_image; ?>" alt="rating">
+					</div>
+				<?php endif; ?>
 				<h3><?php echo $title; ?></h3>
 				<div class="position"><?php echo $position; ?></div>
 				<div class="testimonial"><?php echo $content; ?></div>
