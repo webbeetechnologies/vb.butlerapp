d104b491ae5e88f9949c35c2ef0f551871f65b71
bondan mintardjo <bondan@senta.nu>
2023-10-04 10:13:45 +0300

    hide update notif in plugin section

--- functions.php
+++ functions.php
@@ -624,3 +624,15 @@ function fire_footer() {
 	<?php } ?> 
 <?php }
 do_action( 'fire_footer', 'admin_print_footer_scripts' );
+
+/***********************************************
+  Remove WP update notification
+************************************************/
+
+// hide update notifications
+function remove_core_updates(){
+	global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
+add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
+add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
