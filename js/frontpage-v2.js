0459c507150a5891e482c408f2985a1cd03a6460
bondan mintardjo <bondan@senta.nu>
2023-10-06 17:32:10 +0300

    disable hover in faq

--- js/frontpage-v2.js
+++ js/frontpage-v2.js
@@ -341,6 +341,7 @@ $(document).ready(function () {
         }
         */
     });
+    /*
     if ($(window).width() > breakpoint.medium) {
         var delay=1000, setTimeoutConst;
         $('.faq-v2:not(".faq-active")').hover(function() {
@@ -353,6 +354,7 @@ $(document).ready(function () {
             $(this).removeClass('hover');
         });
     }
+    */
 
     /*=======================================================================================
     * KNOWLEDGE BASE: ACCORDION
