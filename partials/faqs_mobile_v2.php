560057f60d0f334ca15eb2062c67535022d503bf
bondan mintardjo <bondan@senta.nu>
2023-10-07 09:51:48 +0300

    try if in faq loop

--- partials/faqs_mobile_v2.php
+++ partials/faqs_mobile_v2.php
@@ -9,39 +9,38 @@
 		); ?>
 		<!-- START THE FAQ LOOP -->
 		<?php $loop = new WP_Query( $args ); ?>
+        <?php if ($loop->have_posts() )):?>
 		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 		<div class="faq faq-v2">
-			<div class="faq-inner">
-				<div class="faq-flipper"></div>
-				<div class="heading-content-area">
-					<h3><?php the_title(); ?></h3>
-					<!-- content area -->
-                    <?php /*
-					<div>
-						<div class="faq-content-area">
-							<div class="faq-content-v2">
-								<?php the_content(); ?>
-							</div>
-							<div class="faq-footer">
-								<div>
-									<h5><?php the_field('faq_name') ?></h5>
-									<div class="position"><?php the_field('faq_position') ?></div>
-								</div>
-								<div>
-									<a href="<?php the_field('faq_contact_link') ?>">Kontaktieren <i class="fas fa-arrow-right"></i></a>
-								</div>
-							</div>
-						</div>
-					</div>
-                    */ ?>
-				</div>
+            <div class="heading-content-area">
+                <h3><?php the_title(); ?></h3>
+                <!-- content area -->
+                <?php /*
+                <div>
+                    <div class="faq-content-area">
+                        <div class="faq-content-v2">
+                            <?php the_content(); ?>
+                        </div>
+                        <div class="faq-footer">
+                            <div>
+                                <h5><?php the_field('faq_name') ?></h5>
+                                <div class="position"><?php the_field('faq_position') ?></div>
+                            </div>
+                            <div>
+                                <a href="<?php the_field('faq_contact_link') ?>">Kontaktieren <i class="fas fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                */ ?>
+            </div>
 
-				<div class="faq-arrow-flipper">
-					<i class="fas fa-chevron-down"></i> 
-				</div>
-			</div>
+            <div class="faq-arrow-flipper">
+                <i class="fas fa-chevron-down"></i> 
+            </div>
 		</div>
 		<?php endwhile; wp_reset_postdata(); ?>
+        <?php endif; ?>
 		 <!-- END THE FAQ LOOP -->
 	</div>
 </div>
